2257e1befd9f00d3e177357d4e663b2926805bc8
Yashraj Patil <yashrajpatil2206@gmail.com>
2025-04-08 18:50:35 +0530

    Sign up, Login, Logout fully functional

--- videointuit/videointuit/views.py
+++ videointuit/videointuit/views.py
@@ -6,4 +6,7 @@ def index(request):
         'login_url': reverse('login'),
         # ... any other variables for your index.html ...
     }
+    if request.user.is_authenticated:
+        logout_url = reverse('logout')
+        context['logout_url'] = logout_url
     return render(request, 'index.html', context)
